fea29a625132bbf91c0de622443b0c48de07377c
Shakthi Karuppusamy S <54658231+shakthi26@users.noreply.github.com>
2023-12-24 12:23:57 +0530

    Update GenerateAutoNumber.cs

--- Training.Plugins/GenerateAutoNumber.cs
+++ Training.Plugins/GenerateAutoNumber.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//
+// hello
 namespace Training.Plugins
 {
     public class GenerateAutoNumber : IPlugin
